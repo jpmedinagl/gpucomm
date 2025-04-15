6a7d305ba991b8378be6ad00f978fddcf61f8520
jpmedinagl <jpmedinagl@gmail.com>
2025-04-15 12:59:20 -0400

    removing extra

--- modules/peer2.cu
+++ modules/peer2.cu
@@ -13,12 +13,7 @@ int main()
     
     // Socket setup
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(PORT),
-        .sin_addr.s_addr = INADDR_ANY
-    };
-
+    
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
