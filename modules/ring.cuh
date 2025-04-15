e6d66f3ff41512ba2337127f8d482273a35a73a6
jpmedinagl <jpmedinagl@gmail.com>
2025-04-15 12:43:59 -0400

    fixing linking

--- modules/ring.cuh
+++ modules/ring.cuh
@@ -25,6 +25,17 @@ public:
 
     // RingBuffer(void * buf, size_t num_chunks);
 
+    RingBufferRemoteInfo export_metadata() const {
+        return {
+            buffer,
+            // &head,
+            &tail,
+            head,
+            tail,
+            size
+        };
+    }
+
     __device__ void init(void* buf, size_t num_chunks) {
         buffer = buf;
         head = buf;
@@ -33,12 +44,43 @@ public:
         count = 0;
     }
 
-    RingBufferRemoteInfo export_metadata() const;
+    __device__ bool is_empty() const {
+        return count == 0;
+    }
+    __device__ bool is_full() const {
+        return count == size;
+    }
 
-    __device__ bool is_empty() const;
-    __device__ bool is_full() const; 
-    __device__ bool dequeue(void* out_chunk);
-    __device__ bool enqueue(const void * chunk);
-};
+    __device__ bool enqueue(const void * chunk) if (is_full()) {
+            return false;
+        }
+
+        memcpy(tail, data, CHUNK_SIZE);
+
+        uintptr_t offset = (reinterpret_cast<uintptr_t>(tail) - 
+                            reinterpret_cast<uintptr_t>(buffer) + CHUNK_SIZE) % 
+                            (size * CHUNK_SIZE);
+        tail = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(buffer) + offset);
+
+        count--;
+        return true;
+    };
+
+
+    __device__ bool dequeue(void* out_chunk) {
+        if (is_empty()) {
+            return false;
+        }
+
+        memcpy(out_chunk, head, CHUNK_SIZE);
+
+        uintptr_t offset = (reinterpret_cast<uintptr_t>(head) - 
+                            reinterpret_cast<uintptr_t>(buffer) + CHUNK_SIZE) 
+                            % (size * CHUNK_SIZE);
+        head = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(buffer) + offset);
+
+        count--;
+        return true;
+    }
 
 #endif // RING_BUFFER_H
