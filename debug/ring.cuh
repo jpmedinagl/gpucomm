a6f0827cebfd3f8a899305eb480935737de57fc9
jpmedinagl <jpmedinagl@gmail.com>
2025-04-17 11:45:23 -0400

    debugging

---
+++ debug/ring.cuh
@@ -0,0 +1,77 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#include <cstddef>
+#include <cstdint>
+
+#define CHUNK_SIZE 256
+
+struct RingBufferRemoteInfo {
+    uintptr_t buffer_addr;
+    // void ** head_addr_ptr;
+    uintptr_t tail_addr_ptr;
+    uintptr_t head_addr;
+    uintptr_t tail_addr;
+    size_t size;
+};
+
+class RingBuffer {
+public:
+    void * buffer;
+    void * head;
+    void * tail;
+    size_t size;
+
+    // RingBuffer(void * buf, size_t num_chunks);
+
+    __device__ void init(void* buf, size_t num_chunks) {
+        buffer = buf;
+        head = buf;
+        tail = buf;
+        size = num_chunks + 1;
+    };
+
+    __device__ bool is_empty() const {
+        return head == tail;
+    };
+
+    __device__ bool is_full() const {
+        uintptr_t offset = (reinterpret_cast<uintptr_t>(tail) - 
+                            reinterpret_cast<uintptr_t>(buffer) + CHUNK_SIZE) % 
+                            (size * CHUNK_SIZE);
+        void *tmp_tail = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(buffer) + offset);
+        return tmp_tail == head;
+    };
+
+    __device__ bool enqueue(const void * chunk) {
+        if (is_full()) {
+            return false;
+        }
+
+        memcpy(tail, chunk, CHUNK_SIZE);
+
+        uintptr_t offset = (reinterpret_cast<uintptr_t>(tail) - 
+                            reinterpret_cast<uintptr_t>(buffer) + CHUNK_SIZE) % 
+                            (size * CHUNK_SIZE);
+        tail = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(buffer) + offset);
+
+        return true;
+    };
+
+    __device__ bool dequeue(void* out_chunk) {
+        if (is_empty()) {
+            return false;
+        }
+
+        memcpy(out_chunk, head, CHUNK_SIZE);
+
+        uintptr_t offset = (reinterpret_cast<uintptr_t>(head) - 
+                            reinterpret_cast<uintptr_t>(buffer) + CHUNK_SIZE) 
+                            % (size * CHUNK_SIZE);
+        head = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(buffer) + offset);
+
+        return true;
+    };
+};
+
+#endif // RING_BUFFER_H
---
+++ debug/utils.h
@@ -0,0 +1,62 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <ucp/api/ucp.h>
+#include <cuda_runtime.h>
+#include <ucs/memory/memory_type.h>
+#include <ucp/api/ucp_def.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstdio>
+#include <ucp/api/ucp.h>
+#include <ucs/type/status.h>
+
+#include <inttypes.h>
+
+#include <stdio.h>
+
+#define NUM_CHUNKS 1 //0
+#define PORT 12345
+
+#define CHECK_ERROR(_cond, _msg)                                               \
+    do {                                                                       \
+        if (_cond) {                                                           \
+            printf("Failed to %s\n", _msg);                                    \
+            exit(EXIT_FAILURE);                                                \
+        }                                                                      \
+    } while (0)
+
+#define CUDA_CHECK(_func)                                                      \
+    do {                                                                       \
+        cudaError_t _result = (_func);                                         \
+        if (cudaSuccess != _result) {                                          \
+            printf("%s failed: %s\n", #_func, cudaGetErrorString(_result));    \
+            exit(EXIT_FAILURE);                                                \
+        }                                                                      \
+    } while (0)
+
+#define UCS_CHECK(_expr)                                                       \
+    do {                                                                       \
+        ucs_status_t _status = (_expr);                                        \
+        if (UCS_OK != _status) {                                               \
+            printf("%s failed: %s\n", #_expr, ucs_status_string(_status));     \
+            exit(EXIT_FAILURE);                                                \
+        }                                                                      \
+    } while (0)
+
+// Basic socket send, loop to ensure full data transfer
+void socket_send(int sockfd, const void* data, size_t size);
+
+// Basic socket recv, loop to ensure full data transfer
+void socket_recv(int sockfd, void* buffer, size_t size);
+
+void init(ucp_context_h * context, ucp_worker_h * worker);
+
+void create_ep(int sockfd, ucp_worker_h worker, ucp_ep_h * ep);
+
+#endif // UTILS_H
