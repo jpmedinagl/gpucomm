418e985ade870996b94fc2f812a6040f0d94323f
jpmedinagl <jpmedinagl@gmail.com>
2025-05-07 15:42:14 -0400

    queries change

--- map/recv.cu
+++ map/recv.cu
@@ -28,6 +28,17 @@ int main() {
     auto find_ref;
     cudaIpcOpenMemHandle((void**)&find_ref, handle, cudaIpcMemLazyEnablePeerAccess);
 
+    // Define some queries
+    thrust::device_vector<Key> queries = {0, 1, num_keys-1, num_keys, -1};
+    thrust::device_vector<Value> results(queries.size(), -1);
+
+    // Launch a kernel to perform the lookups
+    int* d_queries;
+    int* d_results;
+    cudaMalloc(&d_queries, queries.size() * sizeof(int));
+    cudaMalloc(&d_results, queries.size() * sizeof(int));
+    cudaMemcpy(d_queries, thrust::raw_pointer_cast(queries.data()), queries.size() * sizeof(int), cudaMemcpyDeviceToDevice);
+
     find_in_map_kernel<<<(queries.size() + 255) / 256, 256>>>(find_ref, d_queries, d_results, queries.size());
 
     cudaMemcpy(thrust::raw_pointer_cast(results.data()), d_results, queries.size() * sizeof(int), cudaMemcpyDeviceToHost);
--- map/send.cu
+++ map/send.cu
@@ -26,17 +26,6 @@ int main() {
     auto pairs_begin = thrust::make_zip_iterator(thrust::make_tuple(keys.begin(), values.begin()));
     map.insert(pairs_begin, pairs_begin + num_keys);
 
-    // Define some queries
-    thrust::device_vector<Key> queries = {0, 1, num_keys-1, num_keys, -1};
-    thrust::device_vector<Value> results(queries.size(), -1);
-
-    // Launch a kernel to perform the lookups
-    int* d_queries;
-    int* d_results;
-    cudaMalloc(&d_queries, queries.size() * sizeof(int));
-    cudaMalloc(&d_results, queries.size() * sizeof(int));
-    cudaMemcpy(d_queries, thrust::raw_pointer_cast(queries.data()), queries.size() * sizeof(int), cudaMemcpyDeviceToDevice);
-
     auto find_ref = map.ref(cuco::find);
 
     cudaIpcMemHandle_t handle;
